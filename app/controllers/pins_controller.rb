644ed7f18e2969189c255865cec5e2af1c606ac8
Amar Singh <ummerr@gmail.com>
2014-04-24 07:57:34 -0400

    Broken Paperclip

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -54,7 +54,7 @@ class PinsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
-      params.require(:pin).permit(:description)
+     def pin_params
+      params.require(:pin).permit(:description, :image)
     end
 end
